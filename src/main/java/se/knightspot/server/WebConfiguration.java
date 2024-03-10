e1b97dfa508b04d195d21a8d61bae48159edfbdf
Jonas <jonas@knightspot.se>
2024-03-10 20:05:56 +0100

    Tagit bort onödiga importer

--- src/main/java/se/knightspot/server/WebConfiguration.java
+++ src/main/java/se/knightspot/server/WebConfiguration.java
@@ -1,6 +1,5 @@
 package se.knightspot.server;
 
-import org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
